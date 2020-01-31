6907f5a64a362fb135b02b5df7365e1f6e3291a7
JackCaff <JackCaff@users.noreply.github.com>
2020-01-31 19:10:50 +0000

    added new file for weeklytask3.

---
+++ StringReversed.py
@@ -0,0 +1,6 @@
+# Program allows a user enter a sentance and output every second later of the sentence in reverse
+
+sentence = str (input("Please enter your sentence: "))
+sentence = sentence[::-1] # reverses users sentence (String)
+
+print (sentence[::1]) # outputs the every second letter of the users input 
